57ed673d3c7b307dc91f4082905f6b5f0c45db25
Eric Majerus <varr@Erics-Mac-mini.local>
2016-11-12 14:27:11 -0600

    Renamed routers to controller because router is stupid

--- app.js
+++ app.js
@@ -5,15 +5,29 @@ const logger = require('morgan')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 
-const indexRoutes = require('./server/controller/indexRouter')
-const searchRoutes = require('./server/controller/searchRouter')
-
 const app = express()
 
-//View engine setup
+
+//----------------//----------------//----------------//----------------//----------------
+// Controller/Route Config
+//----------------//----------------//----------------//----------------//----------------
+const indexController = require('./server/controller/indexController')
+const searchController = require('./server/controller/searchController')
+
+app.use('/', indexController)
+app.use('/search/', searchController)
+
+
+//----------------//----------------//----------------//----------------//----------------
+// View Config
+//----------------//----------------//----------------//----------------//----------------
 app.set('views', path.join(__dirname, 'server/views'))
 app.set('view engine', 'hbs')
 
+
+//----------------//----------------//----------------//----------------//----------------
+// Uhhh Stuff I Guess
+//----------------//----------------//----------------//----------------//----------------
 app.use(logger('dev'))
 app.use(cookieParser())
 app.use(bodyParser.json())
@@ -23,12 +37,10 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(favicon(path.join(__dirname, 'public', 'favicon.png')))
 
-app.use('/', indexRoutes)
-app.use('/search/', searchRoutes)
 
-/**
- * Error handlers
- */
+//----------------//----------------//----------------//----------------//----------------
+// App Error Handlers
+//----------------//----------------//----------------//----------------//----------------
 //Catch 404 and forward to error handler
 app.use(function (req, res, next) {
     const err = new Error('Not Found')
