cd1a8686690c84fe6c6243d9dcedee73fa837ccb
Eric Majerus <issmichnicht@gmail.com>
2016-11-18 21:07:24 -0600

    Specifying node version in package.json

--- server/controller/indexController.js
+++ server/controller/indexController.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Chinese'})
+    res.render('index', {title: '干净 gānjìng'})
 })
 
 module.exports = router
