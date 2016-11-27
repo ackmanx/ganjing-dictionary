2af9f0b5ce0b1f489ca1a563947ababab21716a8
Eric Majerus <issmichnicht@gmail.com>
2016-11-26 21:50:20 -0600

    Taylor's changes for responsive grid

--- public/app/results.js
+++ public/app/results.js
@@ -7,16 +7,18 @@ define(function (require) {
             this.am = args.am
         },
         view: (ctrl) =>
-            m('div#results-list',
+            m('section#results-list',
                 ctrl.am.results().length ? m('div.results-counter', `${ctrl.am.results().length} results`) : '',
                 ctrl.am.results().map((entry) =>
                     m('div.listing',
-                        m('span.simplified-col', entry.simplified()),
-                        m('span.pinyin-col', entry.pinyin()),
-                        m('ol.english-col',
-                            entry.english().map((it) => m('li', it))
+                        m('div.simplified-col', entry.simplified()),
+                        m('div.pinyin-col', entry.pinyin()),
+                        m('div.english-col',
+                            m('ol',
+                                entry.english().map((it) => m('li', it))
+                            )
                         ),
-                        entry.hsk() ? m('span.hsk-col', `HSK ${entry.hsk()}`) : ''
+                        entry.hsk() ? m('div.hsk-col', `HSK ${entry.hsk()}`) : ''
                     )
                 )
             )
