30b5bd2b6b42c08a6b985d43880a8dea9f1b29d2
Eric Majerus <issmichnicht@gmail.com>
2017-01-06 21:59:57 -0600

    Whoops, pushed wrong dictionary to prod

--- server/globals.js
+++ server/globals.js
@@ -2,8 +2,8 @@ const path = require('path')
 
 module.exports = {
     db_paths: {
-        giantEffing: path.join(__dirname, '..', 'resources', 'uber-hsk.db'), //dev
-        // giantEffing: path.join(__dirname, '..', 'resources', 'giantEffing.db'),
+        // giantEffing: path.join(__dirname, '..', 'resources', 'uber-hsk.db'), //dev
+        giantEffing: path.join(__dirname, '..', 'resources', 'giantEffing.db'),
         uberHsk: path.join(__dirname, '..', 'resources', 'uber-hsk.db')
     }
 }
